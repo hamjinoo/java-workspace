4f8476f1b65ba8f6d7743ce9bc363d13f83ede48
Jinwoo <hjw3012@naver.com>
2024-07-02 14:31:39 +0900

    Article 예제

---
+++ src/main/java/com/example/firstproject/controller/FirstController.java
@@ -0,0 +1,22 @@
+package com.example.firstproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hi")
+    public String niceToMeetYou(Model model) { // model 객체 받아오기
+        model.addAttribute("username", "이름");
+        return "greetings";
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("username", "이름");
+        return "goodbye";
+    }
+
+}
