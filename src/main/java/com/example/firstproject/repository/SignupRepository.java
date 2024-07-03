a5713b5756ae03d4b20be6de96e9d0a84181e50d
Jinwoo <hjw3012@naver.com>
2024-07-03 09:56:49 +0900

    Signup 삭제 후 복습하기

--- src/main/java/com/example/firstproject/repository/SignupRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.firstproject.repository;
-
-import com.example.firstproject.entity.Signup;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SignupRepository extends CrudRepository<Signup, Long> {
-}
