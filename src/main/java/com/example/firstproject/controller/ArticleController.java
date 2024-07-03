19f2d497990da39615c70e1f128f7e66171857da
Jinwoo <hjw3012@naver.com>
2024-07-03 14:40:48 +0900

    Read 파트 공부

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -3,12 +3,20 @@ package com.example.firstproject.controller;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Controller
+@Slf4j
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
@@ -20,15 +28,45 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form) {
-        System.out.println(form.toString());
+        log.info(form.toString());
         // 1. DTO를 엔티티로 변환
         Article article = form.toEntity();
-        System.out.println(article.toString()); // DTO가 엔티티로 잘 변환되는지 확인 출력
+        log.info(article.toString()); // DTO가 엔티티로 잘 변환되는지 확인 출력
 
         // 2. 레포지토리로 엔티티를 DB에 저장
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString()); // article이 DB에 잘 저장되는지 확인 출력
+        log.info(saved.toString()); // article이 DB에 잘 저장되는지 확인 출력
 
         return "";
+    }
+
+
+    @GetMapping("/articles/{id}")
+    public String showArticle(@PathVariable Long id, Model model) {
+
+        // 1. id를 조회해 데이터 가져오기
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("article", articleEntity);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+
+    @GetMapping("/articles/list")
+    public String listArticles(Model model) {
+
+        // 1. id를 조회해 데이터 가져오기
+        //List<Article> articleEntityList = (List<Article>) articleRepository.findAll();
+        ArrayList<Article> articleEntityList = articleRepository.findAll();
+
+
+        // 2. 모델에 데이터 등록하기
+        model.addAttribute("articleList", articleEntityList);
+
+        // 3. 뷰 페이지 반환하기
+        return "articles/list";
+
     }
 }
