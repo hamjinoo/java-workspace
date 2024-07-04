afc672031ac7bc545a0dec341297d3d9ab3c43bf
Jinwoo <hjw3012@naver.com>
2024-07-04 10:05:45 +0900

    Signup Create 연습 파트 기본작업

---
+++ src/main/java/com/example/firstproject/controller/RegisterController.java
@@ -0,0 +1,37 @@
+package com.example.firstproject.controller;
+
+
+import com.example.firstproject.dto.SignupDTO;
+import com.example.firstproject.entity.Signup;
+import com.example.firstproject.repository.SignupRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Slf4j
+public class RegisterController {
+    @Autowired
+    SignupRepository signupRepository;
+
+    @GetMapping("/register/signup")
+    public String Register() {
+
+        return "register/signup";
+    }
+
+    @PostMapping("/register/signup/create")
+    public String createRegister(SignupDTO signupDTO) {
+
+
+        Signup signup = signupDTO.toEntity();
+
+        Signup save = signupRepository.save(signup);
+
+        log.info(save.toString());
+
+      return "";
+    }
+}
